80702c37713f3ebe02fb03e59f6385b2e790f698
arigree <arigree@iu.edu>
2025-04-01 16:16:23 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(
+  express.static(
+    path.join(__dirname, "views") //
+  )
+);
+
+app.get(
+  "/contact",
+  function (req, res, next) {
+    console.log(req.query);
+    next();
+  },
+  function (req, res, next) {
+    res.send("<p>Limit</p>");
+  }
+);
+
+app.get("/*page/", function (req, res) {
+  res.sendFile(path.join(__dirname, "views/404.html"));
+});
+
+const port = process.env.PORT || 14941;
+
+app.listen(port);
+
+console.log(`Server running: http://localhost:${port}`);
